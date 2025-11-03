9dc9086165aabd2b6f82170d7c052d9d0ebe2929
Kanishka Verma <kanishkav774@gmail.com>
2025-11-03 21:25:48 +0530

    7. Advanced routing. Added clean routing and method based routing

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -7,12 +7,6 @@ import (
 
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		/*
-			// 6.2 Setting security headers
-
-			// Note: This is split across multiple lines for readability. You don't
-			// need to do this in your own code.
-		*/
 		w.Header().Set("Content-Security-Policy",
 		"default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
 
@@ -25,9 +19,6 @@ func secureHeaders(next http.Handler) http.Handler {
 	})
 }
 
-/*
-	// Chapter 6.3. Request logging
-*/
 func (app *application) logRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
@@ -36,35 +27,11 @@ func (app *application) logRequest(next http.Handler) http.Handler {
 	})
 }
 
-/*
-	// 6.4 Panic Recovery
-*/
 func(app *application) recoverPanic(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request)  {
-			
-		/*
-			// 6.4 Panic Recovery
-
-			// Create a deferred function (which will always be run in the event
-			// of a panic as Go unwinds the stack).
-		*/
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request)  {	
 		defer func() {
-
-			/*
-				// 6.4 Panic Recovery
-
-				// Use the builtin recover function to check if there has been a
-				// panic or not. If there has...
-			*/
 			if err := recover(); err != nil {
-				/*
-					// 6.4 Panic Recovery
-
-					// Set a "Connection: close" header on the response.
-				*/
 				w.Header().Set("Connection", "close")
-				// Call the app.serverError helper method to return a 500
-				// Internal Server response.
 				app.serverError(w, fmt.Errorf("%s", err))
 			}
 		}()
